e1df5dc228dec0f57634cc51695231cb3fb1dbe9
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2011-02-07 13:34:39 +0100

    [FIX] keyboard settings CTRL: C- ALT: option ESC/CMD: M-
    Set default encoding to utf8

--- sszuecs/core.el
+++ sszuecs/core.el
@@ -5,3 +5,12 @@
 (require 'auto-dictionary)
 (add-hook 'flyspell-mode-hook '(lambda () (auto-dictionary-mode 1)))
 
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+; Best option in Emacs 23 is to revert to Emacs 22 settings (alt is alt, cmd is meta) with this snippet:
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
