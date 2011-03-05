29dd8b23b0c523dc3afd5270fa18fba9dce4ff79
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2011-03-05 12:52:54 +0100

    init ecb mode

---
+++ sszuecs/ecb.el
@@ -0,0 +1,29 @@
+;;;; no ELPA for ecb 
+; ecb requires loaded cedet
+(add-to-list 'load-path "~/emacs-libs/cedet-1.0")
+(load-file "~/emacs-libs/cedet-1.0/common/cedet.el")
+(load-file "~/emacs-libs/cedet-1.0/speedbar/speedbar.el")
+(load-file "~/emacs-libs/cedet-1.0/eieio/eieio.el")
+(load-file "~/emacs-libs/cedet-1.0/semantic/semantic.el")
+
+(add-to-list 'load-path "~/emacs-libs/ecb-2.40")
+
+; what?
+(global-ede-mode 1) 
+(semantic-load-enable-minimum-features)
+(semantic-load-enable-code-helpers)
+
+(require 'ecb-autoloads)
+(autoload 'ecb "ecb-activate" "Emacs IDE" t)
+(setq ecb-tip-of-the-day nil)
+
+;;; do I want this hook or autoload?
+; (require 'ecb)
+; (add-hook 'ecb-activate-hook
+;           (lambda ()
+;             (let ((compwin-buffer (ecb-get-compile-window-buffer)))
+;               (if (not (and compwin-buffer
+;                             (ecb-compilation-buffer-p compwin-buffer)))
+;                   (ecb-toggle-compile-window -1)))))
+; (ecb-activate)
+
