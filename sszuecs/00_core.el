e1d85fd846d7bb7714abe59f92e666fa9408bbae
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2011-03-05 12:55:35 +0100

    variables for libs and config dir

--- sszuecs/00_core.el
+++ sszuecs/00_core.el
@@ -1,6 +1,11 @@
 ;; Start emacs server. Use `emacsclient -t'.
 (server-start)
 
+(defvar my-libs-dir "~/emacs-libs/" 
+  "Directory for elisp libraries without elpa")
+(defvar my-config-dir "~/.emacs.d/sszuecs/" 
+  "Directory for elisp config files")
+
 ; prefer utf8 encoding
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
