248bab1f6ff84fa82ca104e3578defba7fa1103f
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2011-06-19 23:01:29 +0200

    ported my snippets from textmate bundles to yasnippets

---
+++ plugins/yasnippet-0.6.1c/snippets/text-mode/textile-mode/.yas-setup.el
@@ -0,0 +1,134 @@
+;; .yas-setup.el for Textile.tmbundle-mode
+;; 
+;; --**--
+;; Automatically generated code, do not edit this part
+;; 
+;; Translated menu
+;; 
+(yas/define-menu 'Textile.tmbundle-mode
+                 '(
+                   
+                   
+                   
+                   (yas/submenu "Format"
+                                (;; Bold
+                                 (yas/item "D446156B-1C02-43D0-8824-979FA7AC7C5D")
+                                 ;; Italic
+                                 (yas/item "58E9370D-19C9-466E-A4E0-7D067A9A7C5A")
+                                 ;; Underline
+                                 (yas/item "AF1E916C-AF7F-417D-9776-A11FDF9668C8")
+                                 ;; Strikethrough
+                                 (yas/item "8C3FDC27-98C9-4A15-B13A-22840DA8AE28")
+                                 (yas/separator)
+                                 ;; Superscript
+                                 (yas/item "E6C4BEC8-A5D9-453A-A168-2EEBD50ECDE2")
+                                 ;; Subscript
+                                 (yas/item "2F490EA0-DE03-4A61-989E-D3E853CEAA4A")
+                                 ;; Citation
+                                 (yas/item "877822B7-5DFF-41B5-9114-9DD3F3563D73")
+                                 ;; Color Selection
+                                 (yas/item "11246CED-2010-411E-874B-5F6B60B3C7FE")))
+                   (yas/submenu "Attributes"
+                                (;; Id
+                                 (yas/item "B4767B18-66F1-489A-8E43-9B1982368E03")
+                                 ;; Class
+                                 (yas/item "866D5468-B364-4B66-9EF1-F4372E38CFE9")
+                                 ;; Class and Id
+                                 (yas/item "664A2E0F-7FB4-4FAA-8889-652FA47A817C")
+                                 ;; Language
+                                 (yas/item "2340F25E-B8AB-406C-8745-0B83690D816E")
+                                 ;; Style
+                                 (yas/item "C722F8F6-AB71-422A-A5B3-8C2C92EAE547")
+                                 ;; Color
+                                 (yas/item "BBE99165-35FD-472E-BC77-2BA707A08540")
+                                 ))
+                   
+                   (yas/submenu "Headers"
+                                (;; Heading 1
+                                 (yas/item "114730E7-5F5F-4324-86B7-ED1982F2F27E")
+                                 ;; Heading 2
+                                 (yas/item "F8EFA926-E0FC-4B6F-93C6-030F71F7912E")
+                                 ;; Heading 3
+                                 (yas/item "545B26C2-33F2-40C1-94A1-FCCA0EB9BCA6")
+                                 ;; Heading 4
+                                 (yas/item "425C93B8-5B1A-44E2-88AA-1E910AF6D5CD")
+                                 ;; Heading 5
+                                 (yas/item "1EFB0855-D4EB-49C0-8C44-4F35DD4DA5D2")
+                                 ;; Heading 6
+                                 (yas/item "F4E856BE-6FCE-4933-8E6D-413B8835ABC5")))
+                   (yas/submenu "Lists"
+                                (
+                                 ))
+                   (yas/submenu "Other"
+                                (;; Acronym
+                                 (yas/item "8190AA36-B729-4B14-89CE-9259F106459F")
+                                 ;; Block Quote
+                                 (yas/item "7D7529CF-11F4-43B4-8F8D-ACA66F64FCE7")
+                                 ;; Image
+                                 (yas/item "0A8DF563-5218-4164-9409-48B6000C08EA")
+                                 ;; Linked Image
+                                 (yas/item "9166AEA1-E834-4C95-A638-5A3FE6963488")
+                                 ;; Footnote
+                                 (yas/item "CFF53A3D-4ADC-44F4-B60D-1DB5FAFD7317")))
+                   
+                   ;; Convert Tabs To Table
+                   (yas/item "E0D9B2D7-163F-493B-ACC6-29AF9C5F00BF"))
+                    '("17EAE2C6-6641-4C2B-9AE9-F5958A0884F9"))
+
+;; Unknown substitutions
+;; 
+;; Substitutions for: content
+;; 
+;; # as in Convert Tabs To Table.yasnippet
+;; ${TM_SELECTED_TEXT/(^|$|\t)/|/g}                                                           =yyas> (yas/unknown)
+;; 
+;; # as in Italic.yasnippet
+;; ${TM_SELECTED_TEXT}                                                                        =yyas> (yas/unknown)
+;; 
+;; # as in Bold.yasnippet
+;; `yas/selected-text`                                                                        =yyas> (yas/unknown)
+;; 
+;; 
+
+;; Substitutions for: condition
+;; 
+;; # as in Italic.yasnippet
+;; text.html.textile                                                                          =yyas> (yas/unknown)
+;; 
+;; 
+
+;; Substitutions for: binding
+;; 
+;; # as in Footnote.yasnippet
+;; ^~                                                                                         =yyas> (yas/unknown)
+;; 
+;; # as in Color.yasnippet
+;; @k                                                                                         =yyas> (yas/unknown)
+;; 
+;; # as in Style.yasnippet
+;; ^I                                                                                         =yyas> (yas/unknown)
+;; 
+;; # as in Bold.yasnippet
+;; @b                                                                                         =yyas> (yas/unknown)
+;; 
+;; # as in Superscript.yasnippet
+;; ^$                                                                                      =yyas> (yas/unknown)
+;; 
+;; # as in Strikethrough.yasnippet
+;; @d                                                                                         =yyas> (yas/unknown)
+;; 
+;; # as in Subscript.yasnippet
+;; ^$                                                                                      =yyas> (yas/unknown)
+;; 
+;; # as in Citation.yasnippet
+;; @'                                                                                         =yyas> (yas/unknown)
+;; 
+;; # as in Italic.yasnippet
+;; @i                                                                                         =yyas> (yas/unknown)
+;; 
+;; # as in Underline.yasnippet
+;; @u                                                                                         =yyas> (yas/unknown)
+;; 
+;; 
+
+;; .yas-setup.el for Textile.tmbundle-mode ends here
---
+++ plugins/yasnippet-0.6.1c/snippets/text-mode/yaml-mode/.yas-setup.el
@@ -0,0 +1,39 @@
+;; .yas-setup.el for YAML.tmbundle-mode
+;; 
+;; --**--
+;; Automatically generated code, do not edit this part
+;; 
+;; Translated menu
+;; 
+(yas/define-menu 'YAML.tmbundle-mode
+                 '(;; key: value
+                   (yas/item "D9F46A3E-4F2A-11DA-AFF2-000A95AF0064")
+                   
+                   (yas/separator)
+                   
+                   
+                   (yas/separator)
+                   )
+                    '())
+
+;; Unknown substitutions
+;; 
+;; Substitutions for: content
+;; 
+;; 
+
+;; Substitutions for: condition
+;; 
+;; # as in untitled.yasnippet
+;;                                                                                            =yyas> (yas/unknown)
+;; 
+;; 
+
+;; Substitutions for: binding
+;; 
+;; # as in untitled.yasnippet
+;; ^:                                                                                         =yyas> (yas/unknown)
+;; 
+;; 
+
+;; .yas-setup.el for YAML.tmbundle-mode ends here
