db87d06af1facaf0fd322bc03d32d2aee4840d56
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2011-07-24 19:08:51 +0200

    yasnippet ruby C extension mode import from textmate

---
+++ plugins/yasnippet-0.6.1c/snippets/text-mode/ruby_c_extensions-mode/.yas-setup.el
@@ -0,0 +1,102 @@
+;; .yas-setup.el for Ruby
+;; 
+;; --**--
+;; Automatically generated code, do not edit this part
+;; 
+;; Translated menu
+;; 
+(yas/define-menu 'Ruby
+                 '(;; VALUE attr
+                   (yas/item "609FA853-7AB8-4F57-8034-FA6030FF7C91")
+                   (yas/submenu "Defining Classes"
+                                (;; rb_define_class
+                                 (yas/item "7CDFF6FA-6A85-4617-B0C1-7452E9F03A6F")
+                                 ;; rb_define_module
+                                 (yas/item "5086B566-6CD6-4E6B-817B-9AF481C4950A")
+                                 ;; rb_define_class_under
+                                 (yas/item "0AE59401-1796-4EB5-81ED-2AD6C59095DC")
+                                 ;; rb_define_module_under
+                                 (yas/item "4D9376BC-6942-479C-AEC0-E8FF5E737A4F")
+                                 ;; rb_include_module
+                                 (yas/item "55C94B01-BA92-48EC-8784-F801AD2F9247")
+                                 ;; rb_extend_module
+                                 (yas/item "57FB4AEE-AB7F-4C45-BCF7-D87092A1AE35")
+                                 ;; rb_require
+                                 (yas/item "8D3EC071-795B-4A60-84EB-C419C18F452E")))
+                   (yas/submenu "Defining Methods"
+                                (
+                                 ;; rb_define_method
+                                 (yas/item "41D9DD2A-158A-4347-B92D-5A2A291D2D8F")
+                                 ;; rb_define_module_function
+                                 (yas/item "571CCF53-1990-4A96-A209-4F3AD29A90E2")
+                                 ;; rb_define_singleton_method
+                                 (yas/item "566CD090-06F6-4FA8-812A-BE7381B7FBFB")
+                                 ;; rb_define_alloc_func
+                                 (yas/item "B8940D26-62B5-489D-9AAF-FD78B35CFEA0")
+                                 ;; t_ method implementation
+                                 (yas/item "4CB4C975-1E63-49C7-8017-B75301850C16")
+                                 (yas/submenu "Attributes"
+                                              (;; rb_define_attr (Read/Write)
+                                               (yas/item "05A85364-2D94-400E-83E0-8BC550F902BA")
+                                               ;; rb_define_attr (Read-only)
+                                               (yas/item "F7306863-CC4E-4C89-AA5C-1CF87FE16585")
+                                               ;; rb_define_attr (Write-only)
+                                               (yas/item "4BFADD27-F7A4-4ED6-939A-75D902D18D30")))))
+                   (yas/submenu "Calling Methods"
+                                (;; rb_funcall (Call Function)
+                                 (yas/item "89D51D82-610E-4171-825C-6F4297166A23")
+                                 ;; rb_funcall2 (Call w/Args Array)
+                                 (yas/item "85CBBB85-B8E7-4D92-97A3-7DC9C3DB0B12")))
+                   (yas/submenu "Raising Errors"
+                                (;; rb_raise (Raise Error)
+                                 (yas/item "57BF501D-E165-48DB-BBDB-B06ACA00D57F")
+                                 ;; rb_fatal (Raise Fatal Error)
+                                 (yas/item "1F4DF7B0-5FA2-4ECE-85AB-9EB34496DF34")
+                                 ;; rb_bug (Raise Interpreter Bug)
+                                 (yas/item "8541DEAC-25D3-4C54-90DD-17153676E96B")))
+                   (yas/submenu "Objects"
+                                (;; rb_iv_get
+                                 (yas/item "466C32B5-EC5E-48D5-80F8-E96C80C64431")
+                                 ;; rb_iv_set
+                                 (yas/item "A16CCD96-2BD5-4BB2-AA7E-A4FCF4FC7B8B")))
+                   (yas/submenu "Arrays"
+                                (;; rb_ary_new (Create Array)
+                                 (yas/item "BE5933EB-1E0C-468F-9B4F-7B1657611B6A")
+                                 ;; rb_ary_push (Add to Array)
+                                 (yas/item "EBFA9FEB-3737-42C8-9E82-C4C92A0B68D1")))
+                   (yas/submenu "Strings"
+                                (;; rb_str_new2 (Create String)
+                                 (yas/item "5D5D68E7-3057-4847-AEDE-C84BE466FBB5")
+                                 ;; rb_str_append
+                                 (yas/item "D0C4A61A-2749-4E9F-B338-C17E4CE676A6")
+                                 ;; rb_str_cat
+                                 (yas/item "79B5679D-0F46-4D63-872A-2AAD179F041C")))
+                   (yas/submenu "Symbols"
+                                (;; rb_intern
+                                 (yas/item "DAF0FF7A-7304-4B0A-8A8B-AB5F542537FE")
+                                 ;; rb_to_id
+                                 (yas/item "12285CD3-8E0C-4606-96C2-1C33FB01032F")
+                                 ;; ID2SYM
+                                 (yas/item "2134E7B4-7DC1-43E9-9A7E-ECDCCF0520F7")
+                                 ;; SYM2ID
+                                 (yas/item "74205D93-8B10-4050-BB52-2AC377040181"))))
+                    '())
+
+;; Unknown substitutions
+;; 
+;; Substitutions for: content
+;; 
+;; 
+
+;; Substitutions for: condition
+;; 
+;; # as in VALUE attr.yasnippet
+;;                                                                                            =yyas> (yas/unknown)
+;; 
+;; 
+
+;; Substitutions for: binding
+;; 
+;; 
+
+;; .yas-setup.el for Ruby ends here
