25c180cdc7dea813e7b8c9f736e292df4f3ba8d7
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2011-07-24 19:03:35 +0200

    puppet mode import

---
+++ plugins/yasnippet-0.6.1c/snippets/text-mode/puppet-mode/.yas-setup.el
@@ -0,0 +1,54 @@
+;; .yas-setup.el for Puppet.tmbundle-mode
+;; 
+;; --**--
+;; Automatically generated code, do not edit this part
+;; 
+;; Translated menu
+;; 
+(yas/define-menu 'Puppet.tmbundle-mode
+                 '(
+                   
+                   ;; class
+                   (yas/item "2D3C6C03-B268-42B1-A856-33FB0437B00E")
+                   ;; cron
+                   (yas/item "C22C1778-E61D-40EC-8E06-4B4A5ABEC0E7")
+                   ;; define
+                   (yas/item "22538B08-D4D6-4172-B672-77893A6D8E40")
+                   ;; package
+                   (yas/item "1A1C51EE-AC6D-4F7F-A342-4D6DE1F4FAD3")
+                   ;; file
+                   (yas/item "C84524F5-B248-4A4B-87B9-69DCA93C393F")
+                   ;; user
+                   (yas/item "831FA574-7FC1-47B7-BDD3-67E3E94E8188")
+                   ;; group
+                   (yas/item "44C1B58A-0EF0-4022-A9EF-2D3B432C83FE")
+                   ;; exec
+                   (yas/item "849952B4-0D8A-4B5F-996D-35D531AAAE81")
+                   ;; case
+                   (yas/item "4C76CA5D-F7F7-4354-AAAD-1F8B00D73823")
+                   ;; if
+                   (yas/item "A60AC7AD-8710-4CCB-9A52-D8B84ABDD521")
+                   ;; else
+                   (yas/item "B3E52D1C-3A49-44FA-8D1E-2910567F22C4")
+                   ;; selector
+                   (yas/item "88BA23C0-A6AD-4115-A765-9074BB8C85BD"))
+                    '())
+
+;; Unknown substitutions
+;; 
+;; Substitutions for: content
+;; 
+;; 
+
+;; Substitutions for: condition
+;; 
+;; # as in yumrepo.yasnippet
+;;                                                                                            =yyas> (yas/unknown)
+;; 
+;; 
+
+;; Substitutions for: binding
+;; 
+;; 
+
+;; .yas-setup.el for Puppet.tmbundle-mode ends here
