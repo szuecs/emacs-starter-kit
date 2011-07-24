da2661c8d0960c3e9c2f0d44400f5d3c16d28fbc
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2011-07-24 19:07:19 +0200

    yasnippet macruby mode import from textmate

---
+++ plugins/yasnippet-0.6.1c/snippets/text-mode/macruby-mode/.yas-setup.el
@@ -0,0 +1,49 @@
+;; .yas-setup.el for MacRuby.tmbundle-mode
+;; 
+;; --**--
+;; Automatically generated code, do not edit this part
+;; 
+;; Translated menu
+;; 
+(yas/define-menu 'MacRuby.tmbundle-mode
+                 '((yas/submenu "HotCocoa"
+                                (;; button
+                                 (yas/item "0221C6D5-8C97-44A9-B8D0-1F72E99AC34B")
+                                 ;; popup
+                                 (yas/item "886B9368-E1E5-4C75-9D54-9158C6514795")
+                                 ;; application
+                                 (yas/item "85DD4522-0311-4FA2-B26F-800FE1BCFD94")
+                                 ;; window
+                                 (yas/item "441917AF-9555-481D-99CD-251D2C557E39")
+                                 ;; submenu
+                                 (yas/item "F04D47B8-E673-4442-BA52-F65D775C0673")
+                                 ;; item
+                                 (yas/item "886B8CF5-587E-4BAD-9AB8-88ECDB18A81D")
+                                 ;; separator
+                                 (yas/item "56FE4440-A66B-49BE-92DE-18CF4945B13B")
+                                 ;; table_view
+                                 (yas/item "A4274485-B258-40D9-A650-FCC4BA7D2891")))
+                   
+                   (yas/submenu "Not Working"
+                                (
+                                 )))
+                    '())
+
+;; Unknown substitutions
+;; 
+;; Substitutions for: content
+;; 
+;; 
+
+;; Substitutions for: condition
+;; 
+;; # as in window.yasnippet
+;;                                                                                            =yyas> (yas/unknown)
+;; 
+;; 
+
+;; Substitutions for: binding
+;; 
+;; 
+
+;; .yas-setup.el for MacRuby.tmbundle-mode ends here
