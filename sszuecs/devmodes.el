c9cf7b9d34a22eb937dd7a06cb0a876645f55079
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2011-10-04 15:08:01 +0200

    rspec hook

--- sszuecs/devmodes.el
+++ sszuecs/devmodes.el
@@ -23,3 +23,8 @@
 (autoload 'puppet-mode "puppet-mode" "Puppet Mode." t)
 (add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
 (add-to-list 'interpreter-mode-alist '("puppet" . puppet-mode))
+
+;; rspec
+(add-hook 'rspec-mode-hook
+    #'(lambda ()
+            (setq yas/mode-symbol 'rspec-mode)))
