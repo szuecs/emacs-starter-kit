edf2ac3dc3cc08391fcfce1383b9d5ab2a22b92b
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2012-02-19 12:46:18 +0100

    use html-mode for completion instead of nxhtml-mode

--- sszuecs/nxhtml-config.el
+++ sszuecs/nxhtml-config.el
@@ -4,3 +4,6 @@
 ; TODO nxhtml should be autoloaded so that booting emacs is fast
 ;(add-to-list 'load-path "~/emacs-libs/nxhtml-2.08/nxhtml")
 ;(autoload 'nxhtml "autostart" "nxhtml mode" t)
+
+; use yasnippet
+(yas/define-snippets 'nxhtml-mode nil 'html-mode)
