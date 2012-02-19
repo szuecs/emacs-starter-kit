9a9d621fe87b1f589ad0ce3e0fcd1e11389920f5
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2012-02-19 12:44:32 +0100

    FIX ecb-mode to use latest dev version http://www.emacswiki.org/emacs/EmacsCodeBrowser

--- sszuecs/ecb.el
+++ sszuecs/ecb.el
@@ -1,4 +1,4 @@
-;;;; no ELPA for ecb 
+;;;; no ELPA for ecb
 ; ecb requires loaded cedet
 (add-to-list 'load-path "~/emacs-libs/cedet-1.0")
 (add-to-list 'load-path "~/emacs-libs/cedet-1.0/common")
@@ -11,16 +11,16 @@
 (load-file "~/emacs-libs/cedet-1.0/eieio/eieio.el")
 (load-file "~/emacs-libs/cedet-1.0/semantic/semantic.el")
 
-; autoload <function> <filename> <doc> <interactive f|t|nil> 
+; autoload <function> <filename> <doc> <interactive f|t|nil>
 ;(autoload 'cedet "cedet" "CEDET Mode" t)
 ;(autoload 'speedbar "speedbar" "Speedbar Mode" t)
 ;(autoload 'eieio "eieio" "Eieio Mode" t)
 ;(autoload 'semantic "semantic" "Semantic Mode" t)
 
-(add-to-list 'load-path "~/emacs-libs/ecb-2.40")
+(add-to-list 'load-path "~/emacs-libs/ecb")
 
 ; what?
-(global-ede-mode 1) 
+(global-ede-mode 1)
 (semantic-load-enable-minimum-features)
 (semantic-load-enable-code-helpers)
 
