1c9d430ae381b2f55135477a58100aafeec4797f
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2012-03-07 10:56:01 +0100

    change aspell dictionary with <f8>

--- sszuecs/01_flyspell.el
+++ sszuecs/01_flyspell.el
@@ -1,9 +1,15 @@
-;; spellchecker 
+;; spellchecker
 (require 'auto-dictionary)
 (add-hook 'flyspell-mode-hook '(lambda () (auto-dictionary-mode 1)))
+(setq flyspell-issue-message-flag nil)
 
-;(setq flyspell-mouse-map
-;      (let ((map (make-sparse-keymap)))
-;        (define-key map [down-mouse-3] #'flyspell-correct-word)
-;        map))
-
+; cycle languages on <f8>
+(let ((langs '("en" "de" )))
+  (setq lang-ring (make-ring (length langs)))
+  (dolist (elem langs) (ring-insert lang-ring elem)))
+(defun cycle-ispell-languages ()
+  (interactive)
+  (let ((lang (ring-ref lang-ring -1)))
+    (ring-insert lang-ring lang)
+    (ispell-change-dictionary lang)))
+(global-set-key [f8] 'cycle-ispell-languages)
