a8916b4a21616ff241e99d5b31fc5a700464755c
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2012-05-12 10:10:37 +0200

    add node to js2-mode

--- sszuecs/40_javascript-mode-conf.el
+++ sszuecs/40_javascript-mode-conf.el
@@ -4,6 +4,7 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.node$" . js2-mode))
 
 ;; pretty print json
 (defun esk-pp-json ()
