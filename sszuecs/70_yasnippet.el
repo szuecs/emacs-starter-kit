be4aa6f234199d4b6a67ac2e03657e31648b3c1f
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2012-05-23 16:10:08 +0200

    NOP

--- sszuecs/70_yasnippet.el
+++ sszuecs/70_yasnippet.el
@@ -1,11 +1,6 @@
 ; textmate like tab completion with snippets
-;(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet-0.6.1c")
-;(require 'yasnippet) ;; not yasnippet-bundle
-;(yas/initialize)
-;(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
-
 (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet_szuecs")
-(require 'yasnippet) ;; not yasnippet-bundle
+(require 'yasnippet)
 (setq yas/snippet-dirs '("~/.emacs.d/plugins/yasnippet_szuecs/snippets"))
 (yas/global-mode 1)
 (add-to-list 'ac-sources 'ac-source-yasnippet)
