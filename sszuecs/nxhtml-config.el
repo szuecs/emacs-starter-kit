4e187ef19bc78b169854f967e641e69e2ae761ba
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2012-07-23 03:41:07 +0200

    save

--- sszuecs/nxhtml-config.el
+++ sszuecs/nxhtml-config.el
@@ -1,9 +1,5 @@
 ;; supports inline and internal css, Javascript, (x)html, erb, rhtml
-(load (concat my-libs-dir "nxhtml-2.08/nxhtml/autostart.el"))
-
-; TODO nxhtml should be autoloaded so that booting emacs is fast
-;(add-to-list 'load-path "~/emacs-libs/nxhtml-2.08/nxhtml")
-;(autoload 'nxhtml "autostart" "nxhtml mode" t)
+;(load (concat my-libs-dir "nxhtml-2.08/nxhtml/autostart.el"))
 
 ;; yasnippet can use text-mode/html-mode/ instead of /nxhtml-mode or /nxml-mode
-(yas/define-snippets 'nxhtml-mode nil 'html5-mode)
+;(yas/define-snippets 'nxhtml-mode nil 'html5-mode)
