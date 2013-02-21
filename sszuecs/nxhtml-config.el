65e7f2e6913f71175bb85948433acfa0c47a8ec9
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2013-02-21 21:57:22 +0100

    erb and rhtml templates should use nxhtml erb mode

--- sszuecs/nxhtml-config.el
+++ sszuecs/nxhtml-config.el
@@ -7,3 +7,7 @@
 
 ;; yasnippet can use text-mode/html-mode/ instead of /nxhtml-mode or /nxml-mode
 ;(yas/define-snippets 'nxhtml-mode nil 'html5-mode)
+(setq mumamo-chunk-coloring 'submode-colored)
+(mapcar '(lambda (binding) (add-to-list 'auto-mode-alist binding))
+        '(("\\.rhtml$" . eruby-nxhtml-mumamo-mode)
+          ("\\.erb$" . eruby-nxhtml-mumamo-mode)))
