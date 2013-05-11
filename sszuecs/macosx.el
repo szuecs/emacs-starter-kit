354684b2c9138ef155887d0ca66fb7759fdcddcd
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2013-05-11 21:58:26 +0200

    add path of python-lint osx/homebrew to emacs bash PATH

--- sszuecs/macosx.el
+++ sszuecs/macosx.el
@@ -1,16 +1,15 @@
 (defvar is-darwin (string-equal system-type "darwin"))
 
-; Best option in Emacs 22 is to revert to Emacs 22 settings
-; (alt is alt, cmd is meta) with this snippet:
 (unless (not is-darwin)
   (setq mac-option-key-is-meta nil)
   (setq mac-command-key-is-meta t)
   (setq mac-command-modifier 'meta)
   (setq mac-option-modifier nil)
-
   (setenv "PATH"
     (concat
       "/usr/local/bin" ":"
+      "/usr/local/share/python" ":"
       "/usr/texbin" ":"
       (getenv "PATH")))
 )
+
