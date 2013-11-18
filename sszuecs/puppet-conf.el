1ff3cd8f1bb868db0c2c69c7d61398c12d00a9d3
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2013-11-18 21:28:32 +0100

    default puppet indentation to 4 ws

--- sszuecs/puppet-conf.el
+++ sszuecs/puppet-conf.el
@@ -3,3 +3,4 @@
 (autoload 'puppet-mode "puppet-mode" "Puppet Mode." t)
 (add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
 (add-to-list 'interpreter-mode-alist '("puppet" . puppet-mode))
+(setq puppet-indent-level 4)
