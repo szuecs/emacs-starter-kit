5105851c6f30258b1cc74a47e2a710ce2efbf2da
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2013-12-15 13:30:36 +0100

    get rid of buffer-local-vars warning from mumamo

---
+++ sszuecs/workaround-mumamo-buffer-file-name-warnings.el
@@ -0,0 +1,7 @@
+;; Workaround the annoying warnings:
+;; Warning (mumamo-per-buffer-local-vars):
+;; Already 'permanent-local t: buffer-file-name
+(when (and (>= emacs-major-version 24) (>= emacs-minor-version 2))
+  (eval-after-load "mumamo"
+    '(setq mumamo-per-buffer-local-vars
+           (delq 'buffer-file-name mumamo-per-buffer-local-vars))))
