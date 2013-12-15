f7177ec31de5b4ba4f3ae307fe7750affe4a67f8
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2013-12-15 13:53:43 +0100

    use my patched version of nxhtml

--- sszuecs/nxhtml-config.el
+++ sszuecs/nxhtml-config.el
@@ -1,5 +1,6 @@
 ;; supports inline and internal css, Javascript, (x)html, erb, rhtml
-(load (concat my-libs-dir "nxhtml-2.08/nxhtml/autostart.el"))
+;(load (concat my-libs-dir "nxhtml-2.08/nxhtml/autostart.el"))
+(load (concat my-libs-dir "nxhtml/autostart.el"))
 
 ;; yasnippet can use text-mode/html-mode/ instead of /nxhtml-mode or /nxml-mode
 ;(yas/define-snippets 'nxhtml-mode nil 'html5-mode)
