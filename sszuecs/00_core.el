0c2aa7ed53fa0f27a3b53ab2e36bc65a94c4dd44
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2014-10-31 19:11:00 +0100

    add ido, occur search config and some shortcuts

--- sszuecs/00_core.el
+++ sszuecs/00_core.el
@@ -16,12 +16,75 @@
 ; PATH
 (push "/usr/local/bin" exec-path)
 
+; do not block input events
+(setq redisplay-dont-pause t)
+
+; C-<f11> maximize active window
+(global-set-key (kbd "C-<f11>") 'maximize-window)
+; C-<f10> resize window
+(global-set-key (kbd "C-<f10>") 'resize-windows)
+
+; get rid of confirmations
+(setq confirm-nonexistent-file-or-buffer nil)
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(setq kill-buffer-query-functions
+  (remq 'process-kill-buffer-query-function
+         kill-buffer-query-functions))
+
+;; ido
 ; fuzzy matching within minibuffer
 (require 'ido)
 (setq ido-enable-flex-matching t)
+;  disable URL find file at point support
+(setq ido-use-url-at-point t)
+; magic guess
+(setq ido-use-filename-at-point 'guess)
+; sort buffer
+(setq ido-file-extensions-order '(".org" ".txt" ".pp" ".rb" ".py" ".emacs" ".xml" ".el" ".ini" ".cfg" ".cnf" ".r" ".c" ".h" ".cpp" ".clj" ".java"))
+; get rid of confirmations
+(setq ido-create-new-buffer 'always)
 
 ; tramp
 (setq tramp-default-method "ssh")
 
-; compile mode
+; use tex style input methods, such that we can use utf-8 chars easily
+(set-input-method "TeX")
+  ; you have to (toggle-input-method) if you want to use TeX style input method
+
+; regex builder
+(require 're-builder)
+(setq reb-re-syntax 'string)
+
+; use compile mode
 (require 'compile)
+
+; use <f1> to get a shell
+(global-set-key [f1] 'eshell)
+
+; occur-mode setup
+; use <f2> to get buffer search with occur-mode
+; use C-<f2> to search all buffers with the same mode
+(global-set-key [f2] 'multi-occur-in-matching-buffers)
+
+(eval-when-compile
+  (require 'cl))
+
+(defun get-buffers-matching-mode (mode)
+  "Returns a list of buffers where their major-mode is equal to MODE"
+  (let ((buffer-mode-matches '()))
+   (dolist (buf (buffer-list))
+     (with-current-buffer buf
+       (if (eq mode major-mode)
+           (add-to-list 'buffer-mode-matches buf))))
+   buffer-mode-matches))
+
+(defun multi-occur-in-this-mode ()
+  "Show all lines matching REGEXP in buffers with this major mode."
+  (interactive)
+  (multi-occur
+   (get-buffers-matching-mode major-mode)
+   (car (occur-read-primary-args))))
+
+;; global key for `multi-occur-in-this-mode' - you should change this.
+(global-set-key (kbd "C-<f2>") 'multi-occur-in-this-mode)
