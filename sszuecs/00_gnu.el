2601a1db90a9891445105c95961254213171b2bd
Sandor Szuecs <sandor.szuecs@zalando.de>
2017-02-28 13:14:06 +0100

    fix accent chars as written in https://bugs.launchpad.net/emacs-snapshot/+bug/1251176

--- sszuecs/00_gnu.el
+++ sszuecs/00_gnu.el
@@ -3,4 +3,6 @@
 (unless (not is-gnu-linux)
   (setenv "PATH" (concat (getenv "PATH") ":/usr/share/go/bin:/usr/local/go/bin:/home/sszuecs/go/bin"))
   (setenv "GOROOT" "/usr/share/go")
+  ; make accent work again
+  (require 'iso-transl)
 )
