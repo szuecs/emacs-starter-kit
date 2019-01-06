59f27b0e59f2cb529a0373cf1e7fc4160e95d0cd
Sandor Szücs <sandor.szuecs@zalando.de>
2019-01-06 14:31:46 +0100

    drop as long marmelade is not having a valid tls cert..
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- init.el
+++ init.el
@@ -38,7 +38,7 @@
 
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-  ("marmalade" . "https://marmalade-repo.org/packages/")
+  ;("marmalade" . "https://marmalade-repo.org/packages/")
   ("melpa" . "https://melpa.org/packages/")
   ))
 ;(dolist (source '(
