f40ee14d68b02fa67c68c275345dbd2f5fe98b5b
Sandor Szücs <sandor.szuecs@zalando.de>
2019-11-05 10:42:58 +0100

    fix yasnippets
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- sszuecs/70_yasnippet.el
+++ sszuecs/70_yasnippet.el
@@ -1,6 +1,13 @@
 ; textmate like tab completion with snippets
-(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet_szuecs")
+
+; https://github.com/joaotavora/yasnippet/blob/master/README.mdown#installation
+(add-to-list 'load-path "~/emacs-libs/yasnippet")
+
+(setq yas-snippet-dirs
+      '(
+        "~/.emacs.d/plugins/yasnippet_szuecs/snippets"
+        ))
+
 (require 'yasnippet)
-(setq yas/snippet-dirs '("~/.emacs.d/plugins/yasnippet_szuecs/snippets"))
-(yas/global-mode 1)
+(yas-global-mode 1)
 (add-to-list 'ac-sources 'ac-source-yasnippet)
